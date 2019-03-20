55a1064b44ef4696841aea397d15e3a435721762
Chancec417 <chancec417@gmail.com>
2019-03-20 15:30:16 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+//console.log("Yee ha to package");
+
+const chalk = require('chalk');
+
+console.log(chalk.blue("Hello World"));
+
+var arr = [1, 1, 2, 2, 3, 5]
+ 
+newAry = (require("uniq")(arr));
+console.log(arr);
+ 
